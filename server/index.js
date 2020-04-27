3ba5af4a27b32a543da4066b5e506cc9188c8b04
cnrd <hello@conradweise.com>
2020-04-27 18:45:22 +0200

    added cors

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 const path = require('path')
 const express = require('express')
+const cors = require('cors')
 const helmet = require('helmet')
 const WebSocket = require('ws')
 
 const app = express()
 
+app.use(cors())
 app.use(helmet())
 
 app.get('/connected', (req, res) => {
