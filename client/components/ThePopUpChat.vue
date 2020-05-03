c54accbff1adfc256a9022ca75156af8167111c2
cnrd <hello@conradweise.com>
2020-05-03 13:02:35 +0200

    maybe scroll fix?

--- client/components/ThePopUpChat.vue
+++ client/components/ThePopUpChat.vue
@@ -56,6 +56,7 @@ export default {
       project: 'ui/project'
     }),
     filteredMessages () {
+      this.scroll()
       return this.messages.filter((message) => message.payload.location === this.location)
     },
     placeholder () {
